a6c911e081d3d351d544012f962c66f989b2c383
Andreas Juul Willumsen <andr532c@edu.eal.dk>
2018-04-25 10:45:13 +0200

    interface made and implemented to a degree

--- Magicnote/Domain/LegalArea.cs
+++ Magicnote/Domain/LegalArea.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Magicnote.Domain
 {
-    class LegalArea
+    class LegalArea: IArea
     {
+
+        //List<SubLegalArea> subAreas = new List<SubLegalArea>();
+        List<string> subAreas = new List<string>();
+        
+
+        public List<string> SelectionList(string Main)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                subAreas.Add("list" + i);
+            }
+
+            // DBHandler.GetSubAreas(Main);
+            // foreach area in DBsubarea
+            //{
+            //subAreas.Add(DBM subarea)
+            //}
+            //return List<SubLegalArea>;
+            return subAreas;
+        }
+
+        public void select(string Main)
+        {
+            SubLegalArea.SelectionList();
+        }
     }
 }
--- Magicnote/Domain/SubLegalArea.cs
+++ Magicnote/Domain/SubLegalArea.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Magicnote.Domain
 {
-    class SubLegalArea
+    public class SubLegalArea : IArea
     {
+        //List<Paragraf> subAreas = new List<Paragraf>();
+        List<string> Paragraf = new List<string>();
+
+
+        public List<string> SelectionList(string Main)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Paragraf.Add("list" + i);
+            }
+
+            // DBHandler.GetSubAreas(Main);
+            // foreach area in DBsubarea
+            //{
+            //subAreas.Add(DBM subarea)
+            //}
+            //return List<SubLegalArea>;
+            return Paragraf;
+        }
+        public void Select(String paragraf)
+        {
+
+        }
     }
 }
