420459a97c5a22d73bd3737337c81ceb923cea87
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-04-25 13:32:44 +0200

    somethings

--- Magicnote/Domain/DbManager.cs
+++ Magicnote/Domain/DbManager.cs
@@ -14,6 +14,37 @@ namespace Magicnote.Domain
 
         public List<Paragraph> GetParagraphs(int number)
         {
+            SubLegalArea subLegalArea = new SubLegalArea();
+            using (SqlConnection connection = new SqlConnection(Conn("Database")))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("SP_GetParagraphs", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    return subLegalArea.Paragraphs;
+                }
+                while (reader.Read())
+                {
+                    Paragraph paragraph = new Paragraph
+                    {
+                        ParagraphNumber = (int) reader["ParagraphNumber"],
+                        Headline = reader["Headline"] as string,
+                        Lawtext = (string) reader["Lawtext"]
+                    };
+
+                    subLegalArea.Paragraphs.Add(paragraph);
+                }
+            }
+            return subLegalArea.Paragraphs;
+        }
+
+        public List<Paragraph> GetMainAreas(int number)
+        {
             List<Paragraph> paragraphs = new List<Paragraph>();
             using (SqlConnection connection = new SqlConnection(Conn("Database")))
             {
@@ -41,6 +72,35 @@ namespace Magicnote.Domain
                 }
             }
             return paragraphs;
+        }
+
+        public List<SubLegalArea> GetSubAreas(int number)
+        {
+            MainLegalArea mainLegalArea = new MainLegalArea();
+            using (SqlConnection connection = new SqlConnection(Conn("Database")))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("SP_GetSubArea", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    return mainLegalArea.SubLegalAreas;
+                }
+                while (reader.Read())
+                {
+                    SubLegalArea subLegalArea = new SubLegalArea
+                    {
+                        Paragraph = (Paragraph) reader["ParagraphNumber"],
+                    };
+
+                    mainLegalArea.SubLegalAreas.Add(subLegalArea);
+                }
+            }
+            return mainLegalArea.SubLegalAreas;
         }
     }
 }
