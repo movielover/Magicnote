46240b680002597bfed041f765e044d2352793b4
Henrik Laurits Pedersen <henr985a@edu.eal.dk>
2018-04-25 11:01:59 +0200

    codestandard

---
+++ Magicnote/Domain/DbManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Magicnote.Domain
+{
+    public class DbManager
+    {
+        public static string Conn(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<Paragraf> GetParagraphs(int number)
+        {
+            using (SqlConnection connection = new SqlConnection(Conn("Database")))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("SP_GetParagraphs", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
