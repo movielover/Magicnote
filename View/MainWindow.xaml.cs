87975c7d24a68466c59a078dbbdaba72eea88de2
Henrik Laurits Pedersen <henr985a@edu.eal.dk>
2018-05-01 09:36:43 +0200

    implementeret ui fra daniels projekt

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -25,5 +25,30 @@ namespace View
             InitializeComponent();
             DataContext = new MainViewModel();
         }
+        private void ButtonImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".png",
+                Filter =
+                    "Image files (*.png;*.jpeg;*.jpg;*.gif)|*.png;*.jpeg;*.jpg;*.gif|" +
+                    "PNG Files (*.png)|*.png|" +
+                    "JPEG Files (*.jpeg)|*.jpeg|" +
+                    "JPG Files (*.jpg)|*.jpg|" +
+                    "GIF Files (*.gif)|*.gif|" +
+                    "All files (*.*)|*.*"
+            };
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result != true) return;
+            // Open document
+            ImageBox.Source = new ImageSourceConverter().ConvertFromString(dlg.FileName) as ImageSource;
+        }
     }
 }
+
