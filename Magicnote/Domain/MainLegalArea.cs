1fdfc7aff959f774ed22b34252f39e494599bbea
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-05-03 14:17:31 +0200

    d,.slæafsjbdho

--- Magicnote/Domain/MainLegalArea.cs
+++ Magicnote/Domain/MainLegalArea.cs
@@ -1,20 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace Magicnote.Domain
+﻿namespace Magicnote.Domain
 {
     public class MainLegalArea
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
-
-        public List<MainLegalArea> MainLegalAreas = new List<MainLegalArea>();
-
-        public List<SubLegalArea> SubLegalAreas { get; set; }
-
-        public List<MainLegalArea> GetMainLegalAreas()
-        {
-            return MainLegalAreas;
-        }
     }
 }
