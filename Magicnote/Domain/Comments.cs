938a81d8e40829e4668f00cc4c614c5039544948
Tue Thomas Ha Minh Son <thom875f@edu.eal.dk>
2018-05-04 09:32:01 +0200

    Created Class Comments. Created properties.

---
+++ Magicnote/Domain/Comments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicnote.Domain
+{
+    public class Comments
+    {
+        public Paragraph ReferenceToParagraph{ get; set; }
+    }
+}
