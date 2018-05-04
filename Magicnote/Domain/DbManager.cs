437a40a4482226417abafd47023a60b50fe84379
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-05-04 09:43:22 +0200

    Readded DbManager

---
+++ Magicnote/Domain/DbManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Magicnote.Domain
+{
+    public class DbManager
+    {
+        internal MainLegalArea MainLegalArea = new MainLegalArea();
+        private const string ConnectionString =
+            "Server=EALSQL1.eal.local; Database=DB2017_B13; User Id=USER_B13; Password=SesamLukOp_13;";
+
+        public List<MainLegalArea> GetMainLegalAreas()
+        {
+            List<MainLegalArea> mainLegalAreas = new List<MainLegalArea>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("dbo.SP_GetMainLegalAreas", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    MainLegalArea = new MainLegalArea
+                    {
+                        Id = (int)reader["PK_MA_ID"],
+                        Title = (string)reader["MA_Title"]
+                    };
+                    mainLegalAreas.Add(MainLegalArea);
+                }
+                return mainLegalAreas;
+            }
+        }
+
+        public List<SubLegalArea> GetSubAreas(int number)
+        {
+            List<SubLegalArea> subLegalAreas = new List<SubLegalArea>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SP_GetSubLegalAreas", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.Add(new SqlParameter("@FK_MA_ID", number));
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    return subLegalAreas;
+                }
+                while (reader.Read())
+                {
+                    SubLegalArea subLegalArea = new SubLegalArea
+                    {
+                        Id = (int)reader["PK_SA_ID"],
+                        Title = (string)reader["SA_Title"]
+                    };
+
+                    subLegalAreas.Add(subLegalArea);
+                }
+            }
+            return subLegalAreas;
+        }
+        public List<Paragraph> GetParagraphs(int number)
+        {
+            List<Paragraph> paragraphs = new List<Paragraph>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SP_GetParagraphs", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    return paragraphs;
+                }
+                while (reader.Read())
+                {
+                    Paragraph paragraph = new Paragraph
+                    {
+                        ParagraphNumber = (int) reader["ParagraphNumber"],
+                        Headline = reader["Headline"] as string,
+                        Lawtext = (string) reader["Lawtext"]
+                    };
+
+                    paragraphs.Add(paragraph);
+                }
+            }
+            return paragraphs;
+        }
+    }
+}
