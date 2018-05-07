9c3b99212e1f5b35d50067d41bc67e5c3973df5d
Tue Thomas Ha Minh Son <thom875f@edu.eal.dk>
2018-05-07 10:19:09 +0200

    Created GetParagraphs

--- Magicnote/Domain/Paragraph.cs
+++ Magicnote/Domain/Paragraph.cs
@@ -13,26 +13,5 @@ namespace Magicnote.Domain
         public string Headline { get; set; }
 
         public string Lawtext { get; set; }
-
-        public void UpdateParagraf()
-        {
-
-        }
-
-        public string ParagraphNode()
-        {
-            return ParagraphNumber.ToString() + " " + Headline;
-        }
-
-        public List<string> ParagraphList(string SubArea)
-        {
-            List<string> ParagraphList = new List<string>();
-            foreach (object Paragraph in SubArea)
-            {
-                ParagraphList.Add(ParagraphNode());
-            }
-            return ParagraphList;
-        }
-
     }
 }
