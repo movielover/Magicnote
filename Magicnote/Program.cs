f49b9be8b6bf1e1414af926899b3b80a5ca1b7ce
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-05-08 13:30:08 +0200

    refactor code

--- Magicnote/Program.cs
+++ Magicnote/Program.cs
@@ -1,25 +1,23 @@
-﻿using Magicnote.Domain;
-using Magicnote.ViewModel;
-using System;
-
-namespace Magicnote
+﻿namespace Magicnote
 {
     internal class Program
     {
-        static Paragraph para = new Paragraph();
-        static MainViewModel MVM = new MainViewModel();
+        //private static readonly MainViewModel Mvm = new MainViewModel();
+
         private static void Main()
         {
-            MVM.GetParagraphs(1);
-            string text = MVM.Paragraphs[0].Lawtext;
+            //Mvm.GetParagraphs(1);
+            //string text = MVM.Paragraphs[0].Lawtext;
 
-            Console.WriteLine(text);
-            Console.ReadLine();
-
+            //Console.WriteLine(text);
+            //Console.WriteLine();
+            //foreach (Paragraph mvmParagraph in Mvm.Paragraphs)
+            //{
+            //    Console.WriteLine(mvmParagraph.Headline);
+            //    Console.WriteLine(mvmParagraph.Lawtext);
+            //}
+            //Console.ReadLine();
         }
-
-
-
 
         //private static void Run()
         //{
@@ -29,7 +27,5 @@ namespace Magicnote
         //    {
         //        Console.WriteLine(SubLegalArea.Title); 
         //    }
-
     }
 }
-
