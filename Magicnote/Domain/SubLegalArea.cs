148446fd1c3799226f78b9ea350a49464821c00e
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-05-10 11:07:56 +0200

    Changed GetSubAreas back

--- Magicnote/Domain/SubLegalArea.cs
+++ Magicnote/Domain/SubLegalArea.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace Magicnote.Domain
+﻿namespace Magicnote.Domain
 {
     public class SubLegalArea
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
-
-        public List<SubLegalArea> SubLegalAreas = new List<SubLegalArea>();
     }
 }
