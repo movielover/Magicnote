18e2aea2bf17be419fc81501f68ad0c9f93a0e52
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-05-15 12:22:01 +0200

    Moved MainViewModel to its own project

--- MainViewModel/MainViewModel.cs
+++ MainViewModel/MainViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 
-namespace Magicnote.ViewModel
+namespace MainViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
@@ -77,3 +77,4 @@ namespace Magicnote.ViewModel
         }
     }
 }
+
