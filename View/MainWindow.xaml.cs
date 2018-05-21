63de8362f18c242a129dd830998b8e22300d76a2
Daniel Cornelsen <dani179b@edu.eal.dk>
2018-05-21 13:32:52 +0200

    d,m.fskldj

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -5,19 +5,14 @@ using ViewModel;
 
 namespace View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            DataContext = new MainViewModel();
+            DataContext = new MenuViewModel();
         }
-
-
 
         private void MakeNote_OnClick(object sender, RoutedEventArgs e)
         {
@@ -26,17 +21,11 @@ namespace View
             Close();
         }
 
-
         private void GetToMenu_Click(object sender, RoutedEventArgs e)
         {
             Menu menu = new Menu();
             menu.Show();
             Close();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
