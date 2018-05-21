165b6ecd467818351529b0001629a803422dc98b
Henrik Laurits Pedersen <henr985a@edu.eal.dk>
2018-05-21 11:42:14 +0200

    startet på noteviewmodel

---
+++ ViewModel/NoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Magicnote.Domain;
+
+namespace ViewModel
+{
+    public class NoteViewModel
+    {
+        public DbManager DbManager;
+        public SubLegalArea SubLegalArea;
+        public Paragraph Paragraph;
+    }
+}
