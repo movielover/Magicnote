1e02aca46d3efbd1c6707b76c859f3848138f561
Henrik <Henrik@DESKTOP-V2GJU4J>
2019-12-18 15:25:02 +0100

    added sqlcheck and more

---
+++ Magicnote/Domain/ParagraphStk.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicnote.Domain
+{
+    public class ParagraphStk
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+
+    }
+}
---
+++ Magicnote/Domain/notions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicnote.Domain
+{
+    public class Notions
+    {
+        //Denne klasse skal definere de begreber der skal bruges på tværs af paragraffer.
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
